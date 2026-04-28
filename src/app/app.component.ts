412829e6079089fa0f3a3466e9f7f19aa096f6c6
AnilKumar-Aratikatla <anilkumara@sourabinfotech.in>
2026-04-28 23:19:47 +0530

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OZO_Website';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    });
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +8,46 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   isCategoryOpen = false;
   isMenuOpen = false;
+  lastScrollTop = 0;
   toggleCategory() {
     this.isCategoryOpen = !this.isCategoryOpen;
   }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    const topHeader = document.getElementById('topHeader');
+    const navbar = document.getElementById('mainNavbar');
+
+    if (scrollTop > this.lastScrollTop) {
+      // 🔽 Scroll down
+      topHeader?.classList.add('hide');
+    } else {
+      // 🔼 Scroll up
+      topHeader?.classList.remove('hide');
+    }
+
+    // Add shadow effect
+    if (scrollTop > 50) {
+      navbar?.classList.add('scrolled');
+    } else {
+      navbar?.classList.remove('scrolled');
+    }
+
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
+  closeMenu() {
+    setTimeout(() => {
+      const navbar = document.getElementById('navbarNav');
+      navbar?.classList.remove('show');
+
+      // 🔥 IMPORTANT FIX
+      this.isMenuOpen = false;
+
+    }, 200);
+  }
+
+
 }
