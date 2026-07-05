ecbc08cbcde3dfe3c581df4c8c6c3c292e72402d
AnilKumar-Aratikatla <anilkumara@sourabinfotech.in>
2026-07-05 16:48:06 +0530

    udpate environtment url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://ozo-backend-2.onrender.com/api'
-    apiUrl: 'http://localhost:3001/api'
+    apiUrl: 'https://ozo-backend-2.onrender.com/api'
+    // apiUrl: 'http://localhost:3001/api'
 }
